4903b82f790e46f584eb92e7952dcb19b524ca1a
Sean Finch <165737121+finchs1@users.noreply.github.com>
2024-09-20 15:09:47 -0400

    Add registration to UserController

--- Backend/src/main/java/studentmanagementsystem/controllers/UserController.java
+++ Backend/src/main/java/studentmanagementsystem/controllers/UserController.java
@@ -1,15 +1,44 @@
-package studentmanagementsystem.controllers;
+package studentmanagementsystem;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
 
 @Controller("/user")
-public class UserController {
-
-    //Test to make sure controller is working
-    @Get("/")
-    public String index() {
-        return "User Controller is working!";
-    }
-
+public class UserController
+{	
+	public String UserRegistrationReply(boolean success)
+	{
+		String reply = "";
+		if (success)
+		{
+			reply = "success";
+		}
+		else
+			reply = "failed";
+		return "{\"status\": \"%s\"}".format(reply);
+	}
+	
+	@Post("/register")
+	public HttpResponse<String> RegisterUser(@Body UserRegistration req)
+	{
+		try {
+			PreparedStatement ps = Application.db.conn.prepareStatement("insert into users values (?, ?, null)");
+			ps.setString(1, req.getEmail());
+	    	Argon2 argon2 = Argon2Factory.create();
+	    	String hash = argon2.hash(10, 65536, 1, req.getPassword().toCharArray());
+			ps.setString(2, hash);
+			ps.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return HttpResponse.ok(UserRegistrationReply(true));
+	}
 }
