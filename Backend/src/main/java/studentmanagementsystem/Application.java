78f063ba19b347ac0556f7ccebf3d9dcd4d699ff
Sean Finch <165737121+finchs1@users.noreply.github.com>
2024-09-20 15:10:48 -0400

    Update Application.java

--- Backend/src/main/java/studentmanagementsystem/Application.java
+++ Backend/src/main/java/studentmanagementsystem/Application.java
@@ -3,8 +3,11 @@ package studentmanagementsystem;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
-
+	public static Database db;
+	
+	
     public static void main(String[] args) {
+    	db = new Database();
         Micronaut.run(Application.class, args);
     }
 }
