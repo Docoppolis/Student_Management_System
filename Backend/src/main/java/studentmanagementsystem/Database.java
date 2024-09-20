b54ea610fa7d06cc29c0fe528f696752a32f1be4
Sean Finch <165737121+finchs1@users.noreply.github.com>
2024-09-20 15:27:04 -0400

    Create Database.java

---
+++ Backend/src/main/java/studentmanagementsystem/Database.java
@@ -0,0 +1,24 @@
+package studentmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Database
+{
+	Connection conn;
+	
+	public Database()
+	{
+		Properties props = new Properties();
+		props.setProperty("user", "dbusername");
+		//props.setProperty("password", "dbpassword");
+		try {
+			conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/sms", props);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
