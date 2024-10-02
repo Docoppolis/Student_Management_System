4c6c598f623e4536bb5da3075c5f6de15311883d
Docoppolis <docoppolis@gmail.com>
2024-10-02 14:04:22 -0400

    Layer isolation for real this time

--- Frontend/Components/home.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-import styles from '@/Components/home.module.css'
-
-function Rectangle({children} : any)
-{
-    return <div className={styles.rectangle}>{children}</div>
-}
-
-export default Rectangle
--- Frontend/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import '@/styles/styles.css'
-import '@/styles/fonts.css'
-
-export default function App({ Component, pageProps } : any)
-{
-    return <Component {...pageProps} />
-}
